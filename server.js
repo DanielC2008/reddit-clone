0819e42117166b50f8fa0602a9dd4509425f1cba
Joseph Daniel Chapdelaine <orvist@gmail.com>
2016-09-15 14:35:21 -0500

    added error handlers for 404 and 500

--- server.js
+++ server.js
@@ -3,18 +3,35 @@
 const express = require('express')
 const app = express()
 const router = require('./routes/index.js')
-
-
-
+const bodyParser = require('body-parser')
 
 
 
 ///////******MIDDLEWARES********\\\\\\\\
-
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 
 
 //////********ROUTES********\\\\\\\
 app.use(router)
+
+
+//////********ERRORS********\\\\\\\
+//404
+app.use((req, res) => {
+	console.log('404');
+	//add 404 render
+})
+
+// All other errors
+app.use((err, req, res, next) => {
+	res.sendStatus(err.status || 500)
+	// error message replicating http server
+	console.error(`[${new Date()}] "${req.method} ${req.url}" Error(${res.statusCode}) ${res.statusMessage}`);
+	res.render('error', {error: err})
+})
+
 
 
 
